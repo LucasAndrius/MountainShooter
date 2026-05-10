bc11b16fcccb5e100e2210086ee2edce0419fb40
Lucas Andrius de Oliveira <devlucasandrius@gmail.com>
2026-05-10 19:51:27 -0300

    adding classes genereted from StarUML

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import pygame as pg
 
 pg.init()
 
-window = pg.display.set_mode(size = (600, 480) )
+window = pg.display.set_mode(size=(800, 480))
 print('setup end')
 
 print('loop start')
+
+# Inicio Mantem janela aberta
 while True:
     # check for all events
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            pg.quit()            # Close window
-            quit()               # end pymage
+            print("Quitting...")
+            pg.quit()  # Close window
+            quit()  # end pymage
+# Fim Mantem janela aberta
