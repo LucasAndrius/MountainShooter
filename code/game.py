3b70a85e93611c560c3e54ff035b9eb2d4c102b7
Lucas Andrius de Oliveira <devlucasandrius@gmail.com>
2026-05-10 21:07:47 -0300

    Creating asset folder, inserting all assets Creating the menu: background, sound and fonts
    Creating the const file

--- code/game.py
+++ code/game.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
 
-import pygame as pg
-
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
-
 
 class Game:
     def __init__(self):
 
-        pg.init()
-        self.window = pg.display.set_mode(size=(800, 480))
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
-
-        # Inicio Mantem janela aberta
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            # check for all events
-            # for event in pg.event.get():
-            #     if event.type == pg.QUIT:
-            #         pg.quit()  # Close window
-            #         quit()  # end pymage
-        # Fim Mantem janela aberta
